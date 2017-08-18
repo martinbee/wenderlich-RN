eb757f20080355270d15958b29e6cb751b2b35f2
Martin Bee <martbee88@gmail.com>
2017-08-18 10:21:19 -0500

    feat: begin details page

--- components/Details/Display.js
+++ components/Details/Display.js
@@ -1,7 +1,104 @@
 import React from 'react';
+import { startCase } from 'lodash';
 
-import { Text } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+} from 'react-native';
 
-const DetailsDisplay = ({ title }) => <Text>{title}</Text>;
+const DetailsDisplay = ({
+  title,
+  img_url,
+  keywords,
+  price_formatted,
+  bedroom_number,
+  property_type,
+  summary,
+  lister_name,
+  lister_url,
+}) => {
+  const price = price_formatted.split(' ')[0];
+
+  const renderTitle = () => (
+    <View style={styles.titleContainer}>
+      <Text style={styles.title} numberOfLines={2}>
+        {title}
+      </Text>
+    </View>
+  );
+
+  const renderCoreInfo = () => (
+    <Text>
+      Price: {price}
+    </Text>
+    <Text>
+      Bedrooms: {bedroom_number}
+    </Text>
+    <Text>
+      Property Type: {startCase(property_type)}
+    </Text>
+  );
+
+  const renderSummary = () => (
+    <Text style={styles.summary} numberOfLines={3}>
+      {summary}
+    </Text>
+  );
+
+  const renderLister = () => (
+    <Text>
+      Lister: {lister_name}
+    </Text>
+  );
+
+  const renderKeywords = () => (
+    <Text>Tags: {keywords}</Text>
+  );
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={{ uri: img_url }} />
+      {renderTitle()}
+      <View style={styles.textContainer}>
+        {renderSummary()}
+        {renderCoreInfo()}
+        {renderLister()}
+        {renderKeywords()}
+      </View>
+    </View>
+  );
+};
 
 export default DetailsDisplay;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  titleContainer: {
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 18,
+    color: '#656565',
+    padding: 10,
+  },
+  image: {
+    width: 400,
+    height: 300,
+  },
+  textContainer: {
+    padding: 10,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+});
--- components/Details/index.js
+++ components/Details/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-//import Display from './Display';
-import { Text } from 'react-native';
+import Display from './Display';
 
 const Details = ({ navigation }) => {
   const { item } = navigation.state.params;
 
-  console.log(item);
-  return <Text>{item.title}</Text>
-  //return <Display {...item} />;
+  return <Display {...item} />;
 };
 
 export default Details;
